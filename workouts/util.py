e84b793d8dd4303c2f5906f68089d42c407733cb
Miles <milesthemeerkat@gmail.com>
2022-03-06 01:40:51 -0800

    Added utility functions; create_workout boilerplate

---
+++ workouts/util.py
@@ -0,0 +1,19 @@
+"""
+Utility functions
+"""
+
+# import pandas as pd
+
+# from workouts.workout import Workout
+
+
+def create_workout() -> str:
+    """
+    Create a workout from tabular data
+
+    Since I'm in control of what data gets entered, I can assume the schema
+    """
+    # get the columns
+    # schema = data.columns.tolist()
+
+    return "Workout"
