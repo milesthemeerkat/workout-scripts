1175ebaa2480801184b2f74ea065ff1e5ee42539
Miles <milesthemeerkat@gmail.com>
2022-03-22 18:09:05 -0700

    wip updating input operations

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Workout app's main start up function
 def main():
     print("Hello World!")
 
+    # TODO: Ask user for input file path
+
 
 if __name__ == "__main__":
     main()
--- workouts/util.py
+++ workouts/util.py
@@ -2,18 +2,34 @@
 Utility functions
 """
 
-# import pandas as pd
+import pandas as pd
 
 from workouts.workout import Workout
 
 
-def create_workout() -> Workout:
+def create_workout(data: pd.DataFrame) -> Workout:
     """
     Create a workout from tabular data
 
     Since I'm in control of what data gets entered, I can assume the schema
     """
-    # get the columns
-    # schema = data.columns.tolist()
+    # sample data:
+    # Date,Knee Pushups,Full Pushups,Squats,Deadlifts,Curls,Leg Bicycle,Leg Lifts
+    # 02/06/2021,Rest,Rest,Rest,Rest,Rest,Rest,Rest
+    # 02/07/2021,20 x 2; 15 x 1,0,25 x 3,(25) 20 x 3,(20) 12 x 3,0,0
+    # 02/08/2021,20 x 1; 15 x 1; 10 x 1,0,25 x 3,(25) 20 x 3,(20) 15 x 3,0,0
 
-    return "Workout"
+    # TODO: This is temp code
+    # get the columns
+    schema = data.columns.tolist()
+
+    return schema
+
+
+def read_csv_input(filepath: str) -> pd.DataFrame:
+    """
+    Read input from csv into pandas dataframe
+
+    TODO: consider other methods of input
+    """
+    return pd.read_csv(filepath)
