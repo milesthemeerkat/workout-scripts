9c08708df43fe912e15f02985f682b33c81c6156
Miles <milesthemeerkat@gmail.com>
2022-03-22 21:40:48 -0700

    wip, adding parse of cell data into Workout objects

--- workouts/util.py
+++ workouts/util.py
@@ -2,28 +2,72 @@
 Utility functions
 """
 
+from datetime import datetime
 import pandas as pd
 
-from workouts.workout import Workout
+from workouts.workout import WorkoutFactory, Workout
+from workouts.constants import EMPTY_VALS
 
 
-def create_workout(data: pd.DataFrame) -> Workout:
+def parse_cell_into_workout(name: str, date: datetime, cell: str) -> Workout:
     """
-    Create a workout from tabular data
-
-    Since I'm in control of what data gets entered, I can assume the schema
+    Parse cell data and create the appropriate Workout object
     """
-    # sample data:
-    # Date,Knee Pushups,Full Pushups,Squats,Deadlifts,Curls,Leg Bicycle,Leg Lifts
-    # 02/06/2021,Rest,Rest,Rest,Rest,Rest,Rest,Rest
-    # 02/07/2021,20 x 2; 15 x 1,0,25 x 3,(25) 20 x 3,(20) 12 x 3,0,0
-    # 02/08/2021,20 x 1; 15 x 1; 10 x 1,0,25 x 3,(25) 20 x 3,(20) 15 x 3,0,0
+    # sanity check, lowercase it all and remove whitespace
+    cell = cell.lower().replace(" ", "")
 
-    # TODO: This is temp code
-    # get the columns
-    schema = data.columns.tolist()
+    # case of no workout data in cell
+    if cell in EMPTY_VALS:
+        # TODO: What do we return if no data? Will None work in dataframe / graph later?
+        return None
 
-    return schema
+    # case of Lift Workout
+    # Ex Lift: (20) 20 x 2; (10) 10 x 2
+    # if there's an x in it, it's a lift (because reps x sets is required to be a Lift)
+    if "x" in cell:
+        weights = reps = sets = []
+        is_sets = False
+
+        for i, _ in enumerate(cell):
+            # (weight)
+            if cell[i] == "(":
+                # grab digits until )
+                weight_str = ""
+
+                while cell[i] != ")":
+                    weight_str += cell[i]
+                    i += 1
+
+                    # error check - bad data
+                    if i > len(cell) - 1:
+                        # TODO: handle this error better
+                        raise Exception(f"Error parsing cell: {cell}")
+
+                weights.append(int(weight_str))
+
+            # x - toggle between sets and reps
+            if cell[i] == "x":
+                is_sets = not is_sets
+
+            # sets and reps
+            # ; - end of this sets / reps
+            digit_str = ""
+            while cell[i] != ";" and i < len(cell) - 1:
+                digit_str += cell[i]
+                i += 1
+
+            if is_sets:
+                sets.append(int(digit_str))
+            else:
+                reps.append(int(digit_str))
+
+        return WorkoutFactory.create_workout(
+            "lift", name=name, date=date, weights=weights, reps=reps, sets=sets
+        )
+
+    # TODO: case of Cardio Workout
+    # Ex Cardio: 20 mins, 10 kmph, 3.0
+    return None
 
 
 def read_csv_input(filepath: str) -> pd.DataFrame:
